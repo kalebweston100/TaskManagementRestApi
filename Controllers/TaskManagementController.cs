0c0929d66d0ecfa7dc9d2ea7fd0e205b058c8585
kalebweston100 <43739811+kalebweston100@users.noreply.github.com>
2024-12-13 10:16:57 -0700

    Initial Commit
    Created structure of the project

---
+++ Controllers/TaskManagementController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskManagementController : ControllerBase
+    {
+        private List<Task> _tasks = new List<Task>();
+
+        [HttpPost(Name = "CreateTask")]
+        public StatusCodeResult Post(string json)
+        {
+            Task? newTask = JsonSerializer.Deserialize<Task>(json);
+            _tasks.Append(newTask);
+            return new StatusCodeResult(200);
+        }
+
+        [HttpGet(Name = "GetTasks")]
+        public string Get()
+        {
+            string json = "";
+            if (_tasks.Count > 0)
+            {
+                json = JsonSerializer.Serialize(_tasks);
+            }
+            else
+            {
+                json = "{}";
+            }
+            return json;
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,13 @@
+namespace TaskManagementRestApi
+{
+    public class Task
+    {
+        public string? title { get; set; }
+
+        public string? description { get; set; }
+
+        public string? priority { get; set; }
+
+        public string? dueDate { get; set; }
+    }
+}
